1828361935dc9d41c86b94fd53e30c64c1a46646
Jess Anthony Tahil <jessanthony.tahil10@gmail.com>
2025-01-03 15:24:43 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,8 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-// import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 
 // Your Firebase project's configuration
 // const firebaseConfig = {
